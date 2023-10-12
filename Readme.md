eecc01aca2f82a8912285edae92e1985c6539987
Hai Ton That <haitt.bkdn@gmail.com>
2023-10-12 17:05:37 +0700

    Second commit

---
+++ Readme.md
@@ -0,0 +1,29 @@
+# Fall Detect
+
+Fall detect using Neural Network
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+
+## Installation
+
+Provide step-by-step instructions on how to get the project set up on a local machine. For example:
+
+1. Clone the mobiFall-v2-dataset:
+
+   ```bash
+   git clone https://github.com/haiton266/mobiFall-v2-dataset.git
+   ```
+
+## Structure
+
+- Falls
+  - `*.txt`
+- notFalls
+  - `*.txt`
+- `create_model.py` (Create model)
+- `main.py` (Load model and predict)
+- `pre1.py` (Prepare data 1 - get from mobiFall-v2-dataset)
+- `pre2.py` (Prepare data 2 - get from mobiFall-v2-dataset)
--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import os
-import fnmatch
+from keras.models import load_model
+import  numpy as np
+model = load_model('save_model.keras')
 
+model.summary()
 
-index = 0
-for i in range(1,32):
-    directory = f"./sub{i}/FALLS"
-    if not os.path.exists(directory):
-        print(f"{directory} is not exist!")
-    else:
-        os.chdir(directory)
-        for folder in os.listdir():
-            os.chdir(f"./{folder}")
-            for file in os.listdir():
-                if fnmatch.fnmatch(file, '*_acc_*'):
-                    with open(file, 'r') as file:
-                        file_contents = file.readlines()[17::4]
-                        processed_contents = [line.split()[1:] for line in file_contents]
-                        final_contents = [' '.join(line) for line in processed_contents]
-                        os.chdir('../../../Falls')
-                        with open(f'{index}.txt', 'w') as file:
-                            for item in final_contents:
-                                file.write(f'{item}\n')
-                            index+=1
-                        os.chdir(f'../sub{i}/FALLS/{folder}')
-            os.chdir('../')
-        os.chdir('../../')
+os.chdir('./notFall')
+
+with open('0.txt', 'r') as file:
+    lines = file.readlines()
+xRealtime = []
+for line in lines:
+    elements = [float(x) for x in line.strip().split(',')]
+    xRealtime.extend(elements)
+xRealtime = xRealtime[:230]
+
+xRealtime = np.array(xRealtime).reshape(1, -1)  # Assuming xRealtime is 1 sample
+
+predictions = model.predict(xRealtime)
+
+class_labels = (predictions > 0.5).astype(int)
+
+print(class_labels)
